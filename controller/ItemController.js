73798175ac01c19fc86998b8504fa42bbe77206e
sjodie1157 <sjodie1157@gmail.com>
2024-03-05 10:18:41 +0200

    Implimented Item Managment service

---
+++ controller/ItemController.js
@@ -0,0 +1,68 @@
+import { getItems, getSingleItem, addItem, updateItem, deleteItem } from "../models/DatabaseItems.js";
+
+export default {
+    getItems: async (req, res) => {
+        try {
+            const items = await getItems()
+            res.json(items)
+        } catch (error) {
+            console.error('Error getting Items');
+            res.status(500).json({ error: 'Internal Server Error'})
+        }
+    },
+    getSingleItem: async (req, res) => {
+        try {
+            const item = await getSingleItem(+req.params.id);
+            if (!item) {
+                return res.status(404).json({ error: "Item not found" });
+            }
+            res.json(item);
+        } catch (error) {
+            console.error("Error getting one item:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    },
+    addItem: async (req, res) => {
+        try {
+            const { prodName, prodPrice, prodDiscription, prodImg, prodCategory, prodQuantity } = req.body;
+            const newItem = await addItem( prodName, prodPrice, prodDiscription, prodImg, prodCategory, prodQuantity);
+            res.status(201).json(newItem);
+        } catch (error) {
+            console.error("Error adding item:", error);
+            res.status(400).json({ error: error.message });
+        }
+    },
+    updateItem: async (req, res) => {
+        try {
+            const existingItem = await getSingleItem(+req.params.id);
+            if (!existingItem) {
+                return res.status(404).json({ error: "Item not found" });
+            }
+
+            const { prodName, prodPrice, prodDiscription, prodImg, prodCategory, prodQuantity } = req.body;
+
+            const updatedFields = {};
+            if (prodName) updatedFields.prodName = prodName;
+            if (prodPrice) updatedFields.prodPrice = prodPrice;
+            if (prodDiscription) updatedFields.prodDiscription = prodDiscription;
+            if (prodImg) updatedFields.prodImg = prodImg;
+            if (prodCategory) updatedFields.prodCategory = prodCategory;
+            if (prodQuantity) updatedFields.prodQuantity = prodQuantity;
+
+            const updatedItem = await updateItem(req.params.id, updatedFields);
+            res.json(updatedItem);
+        } catch (error) {
+            console.error("Error updating an Item:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    },
+    deleteItem: async (req, res) => {
+        try {
+            const deletedItem = await deleteItem(req.params.id);
+            res.json({ message: "Item deleted successfully" });
+        } catch (error) {
+            console.error("Error deleting Item:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    },
+}
---
+++ models/DatabaseItems.js
@@ -0,0 +1,100 @@
+import { createPool } from 'mysql2/promise';
+import { config } from 'dotenv';
+
+config();
+
+// Connection to databse
+const pool = createPool({
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    multipleStatements: true,
+    connectionLimit: 30
+});
+
+// Get all Items
+const getItems = async () => {
+    try {
+        const [result] = await pool.query(`
+            SELECT prodID, prodName, prodPrice, prodDiscription, prodImg, prodCategory, prodQuantity
+            FROM bfqjmxyo9asyeushukci.Products;`);
+        return result;
+    } catch (error) {
+        console.error("Error getting Items:", error);
+        throw error;
+    }
+};
+
+// Get one Item
+const getSingleItem = async (prodID) => {
+    try {
+        const [result] = await pool.query(`
+            SELECT prodID, prodName, prodPrice, prodDiscription, prodImg, prodCategory, prodQuantity
+            FROM bfqjmxyo9asyeushukci.Products
+            WHERE prodID = ?`, [prodID]);
+        return result;
+    } catch (error) {
+        console.error("Error getting Items:", error);
+        throw error;
+    }
+};
+
+// Add an Item
+const addItem = async (prodName, prodPrice, prodDiscription, prodImg, prodCategory, prodQuantity) => {
+    try {
+        const [result] = await pool.query(`
+            INSERT INTO Products ( prodName, prodPrice, prodDiscription, prodImg, prodCategory, prodQuantity)
+            VALUES( ?, ?, ?, ?, ?, ?);`
+            , [prodName, prodPrice, prodDiscription, prodImg, prodCategory, prodQuantity]);
+        const prodID = result.insertId;
+        let item = {
+            prodName,
+            prodPrice,
+            prodDiscription,
+            prodImg,
+            prodCategory,
+            prodQuantity
+        };
+        return { item: await getSingleItem(prodID) };
+    } catch (error) {
+        console.error("Error adding item:", error);
+        throw error;
+    }
+};
+
+// Patch an item
+const updateItem = async (id, updatedFields) => {
+    try {
+        const [existItem] = await getSingleItem(id);
+
+        if (!existItem) {
+            throw new Error("Item not found");
+        }
+
+        const setClause = Object.keys(updatedFields).map(field => `${field} = ?`).join(', ');
+
+        const params = [...Object.values(updatedFields), id];
+
+        const [Item] = await pool.query(
+            `UPDATE bfqjmxyo9asyeushukci.Products
+            SET ${setClause}
+            WHERE ProdID = ?`,
+            params
+        );
+        return { item: await getSingleItem(id) };
+    } catch (error) {
+        console.error("Error updating an Item:", error);
+        throw error;
+    }
+};
+
+// Delete an Item
+const deleteItem = async (id) => {
+    const [item] = await pool.query(`
+        DELETE FROM bfqjmxyo9asyeushukci.Products 
+        WHERE prodID = ?`, [id]);
+    return getItems(item);
+};
+
+export { getItems, getSingleItem, addItem, updateItem, deleteItem }
