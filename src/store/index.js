07c59d361b08850b915fc13b8009718afe48f76a
sjodie1157 <sjodie1157@gmail.com>
2024-03-05 22:19:28 +0200

    added link to cart features

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,31 @@
-import { createStore } from 'vuex'
-const renderLink = 'https://cyber-cecurity.onrender.com/'
+import { createStore } from 'vuex';
+const renderLink = 'https://cyber-cecurity.onrender.com/';
 
 export default createStore({
   state: {
-    users: null
+    users: null,
+    cart: null
   },
   getters: {
   },
   mutations: {
     setUsers(state, value) {
       state.users = value;
+    },
+    setCart(state, value) {
+      state.cart = value;
     }
   },
   actions: {
     async fetchUsers(context) {
       let res = await fetch(`${renderLink}Users`);
       context.commit('setUsers', await res.json());
+    },
+    async fetchCart(context) {
+      let res = await fetch(`${renderLink}Cart/1`);
+      context.commit('setCart', await res.json());
     }
   },
   modules: {
   }
-})
+});
