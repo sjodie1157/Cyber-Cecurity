1f9f162e57e20990b7d8f038afeb51a98a9fe5d5
sjodie1157 <sjodie1157@gmail.com>
2024-03-06 13:28:13 +0200

    added folderStructure

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap'
 
 createApp(App).use(store).use(router).mount('#app')
---
+++ src/views/CartView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        {{ cart }}
+    </div>
+</template>
+
+<script>
+    export default {
+    data() {
+        return {
+        };
+    },
+    computed: {
+        cart() {
+            return this.$store.state.cart;
+        }
+    },
+    async mounted() {
+        try {
+            await this.$store.dispatch('fetchCart');
+        } catch (error) {
+            console.error('Error fetching cart data', error);
+        }
+    }
+    }
+</script>
+
+<style scoped>
+
+</style>
