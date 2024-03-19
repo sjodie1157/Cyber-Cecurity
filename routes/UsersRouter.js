399fac7b6c31c23606240c76f5ee255f0c8c4e1a
sjodie1157 <sjodie1157@gmail.com>
2024-03-19 21:12:54 +0200

    userFix

--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import controller from '../controller/UserController.js';
 import { verifyAToken } from '../middleware/Authenticate.js';
+import { verifyAdminToken } from '../middleware/Authenticate.js';
 
 const router = express.Router();
 
 // Router GET, Post, Delete, Patch for users
-router.get('/', verifyAToken, controller.getUsers)
-router.get('/:id', verifyAToken, controller.getSingleUser)
-router.patch('/:id', verifyAToken, controller.updateUser)
-router.delete('/:id' ,verifyAToken, controller.deleteUser)
+router.get('/', verifyAdminToken, controller.getUsers)
+router.get('/:id', verifyAdminToken, controller.getSingleUser)
+router.patch('/:id', verifyAdminToken, controller.updateUser)
+router.delete('/:id', verifyAdminToken, controller.deleteUser)
 
 // Exported router
 export default router;
