24109c35bb2a24ee34af561497645a259aa2ca66
sjodie1157 <sjodie1157@gmail.com>
2024-03-19 09:54:39 +0200

    reverted back

--- index.js
+++ index.js
@@ -16,41 +16,32 @@ import signinRouter from './routes/SigninRouter.js'
 // User Register Router Import
 import UserRegisterRouter from './routes/UserRegister.js'
 
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 
-import cors from 'cors'
 const port = process.env.PORT || 4500;
 const app = express();
 
 
 
-// app.use(cors({
-//     origin: '',
-//     credentials: true, // Add this line if you want to include credentials
-//     exposedHeaders: 'authorization' // Add this line to expose headers
-// }));
+app.use(cors({
+    origin: 'http://localhost:8080',
+    credentials: true, // Add this line if you want to include credentials
+    exposedHeaders: 'authorization' // Add this line to expose headers
+}));
 
-const corsOptions = {
-    origin: 'http://localhost:8080', // Allow requests from this origin
-    methods: ['GET', 'POST','PATCH' , 'DELETE'],      // Allow only specified methods
-    allowedHeaders: ['authorization'], // Allow only specified headers
-    credentials: true              // Allow sending cookies
-};
-
-
-app.use(cors(corsOptions));
 // This middleware is not needed as the CORS middleware takes care of it.
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     res.header("Access-Control-Allow-Methods", "*");
-//     res.header("Access-Control-Request-Methods", "*");
-//     res.header("Access-Control-Allow-Headers", "*");
-//     res.header("Access-Control-Expose-Headers", "Authorization");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next();
+});
 
 
 app.use(express.json());
