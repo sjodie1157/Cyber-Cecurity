f2df9912f7e7e3eb64bf037d00cdf86ae3298e82
sjodie1157 <sjodie1157@gmail.com>
2024-03-19 10:14:04 +0200

    fixed index.js

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
-import express from 'express'
-// import config from 'dotenv'
-
-// User Router Import
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import UsersRouter from './routes/UsersRouter.js';
+import ItemsRouter from './routes/ItemsRouter.js';
+import CartRouter from './routes/CartRouter.js';
+import SigninRouter from './routes/SigninRouter.js';
+import UserRegisterRouter from './routes/UserRegister.js';
 
-// Item Router Import
-import ItemsRouter from './routes/ItemsRouter.js'
-
-// Cart Router Import
-import CartRouter from './routes/CartRouter.js'
-
-// Signin Router Import
-import signinRouter from './routes/SigninRouter.js'
-
-// User Register Router Import
-import UserRegisterRouter from './routes/UserRegister.js'
-
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-
-
-const port = process.env.PORT || 4500;
 const app = express();
+const port = process.env.PORT || 4500;
 
-
-
+// Middleware to enable CORS with appropriate options
 app.use(cors({
-    origin: 'http://localhost:8080',
-    credentials: true, // Add this line if you want to include credentials
-    exposedHeaders: 'authorization' // Add this line to expose headers
+    origin: 'http://localhost:8080', // Allow requests from this origin
+    credentials: true // Include credentials in CORS request
 }));
 
-// This middleware is not needed as the CORS middleware takes care of it.
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Expose-Headers", "Authorization");
-    next();
-});
-
-
+// Middleware to parse JSON bodies
 app.use(express.json());
+
+// Middleware to parse cookies
 app.use(cookieParser());
+
+// Serve static files from the 'static' directory
 app.use(express.static('./static'));
 
+// Routes
 app.use('/User', UsersRouter);
 app.use('/Register', UserRegisterRouter);
 app.use('/Items', ItemsRouter);
 app.use('/Cart', CartRouter);
-app.use('/login', signinRouter);
+app.use('/login', SigninRouter);
 
-// local host listener
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
