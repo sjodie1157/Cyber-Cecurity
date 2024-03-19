d07564f3a75867776fd2cc264266986dc8bf9534
sjodie1157 <sjodie1157@gmail.com>
2024-03-19 08:40:39 +0200

    changed origin link

--- index.js
+++ index.js
@@ -24,35 +24,9 @@ const port = process.env.PORT || 4500;
 const app = express();
 
 
-// Replace local host link with firebase link when finished
-
-// Middleware - Application level
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     res.header("Access-Control-Allow-Methods", "*");
-//     res.header("Access-Control-Request-Methods", "*");
-//     res.header("Access-Control-Allow-Headers", "*");
-//     res.header("Access-Control-Expose-Headers", "Authorization");
-//     next();
-// })
-
-// app.use(cors({
-//     origin: 'http://localhost:8080',
-//     credentials: true
-// }));
-
-const allowedOrigins = ['http://localhost:8080'];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        // Check if the origin is allowed or if it's undefined (e.g., non-browser requests)
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',
     credentials: true
 }));
 
