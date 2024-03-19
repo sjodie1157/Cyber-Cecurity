e12db8850c061e115a4fd07882d892f374ac18b9
sjodie1157 <sjodie1157@gmail.com>
2024-03-19 09:41:43 +0200

    changed origin link type

--- routes/SigninRouter.js
+++ routes/SigninRouter.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import controller from '../controller/UserController.js';
+import cors from "cors"
 
 const router = express.Router();
+
+const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // Router GET, Post, Delete, Patch for users
 router.post('/', controller.signIn);
